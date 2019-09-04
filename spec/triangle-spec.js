41bf9e3015fcd851f23891a7bcf66f9607ab3309
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-03 23:32:56 -0700

    Create files to test triangle.

---
+++ spec/triangle-spec.js
@@ -0,0 +1,16 @@
+import { Triangle } from './../src/triangle';
+
+describe('Triangle', function() {
+
+  it('should test whether a Triangle has three sides', function() {
+    var triangle = new triangle(3, 4, 5);
+    expect(triangle.side1).toEqual(3);
+    expect(triangle.side1).toEqual(4);
+    expect(triangle.side1).not.toEqual(6);
+  });
+  
+  it('should correctly determine whether three lengths can be made into a triangle', function() {
+    var notTriangle = new Triangle(3,9,22);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+});
